5fc8593cc75d44db02e31b08d532d4e1b11e1c0c
wabham <olaitanwahabhammed@gmail.com>
2024-12-16 17:58:27 +0200

    render now

---
+++ src/page/Homepage.jsx
@@ -0,0 +1,18 @@
+// src/App.js
+
+import React from 'react';
+import Card2 from '../component/Card2'
+
+import Header from '../component/Header';
+const Homepage = () => {
+    return (
+        <div>
+      <Header />
+            <Card2 />
+        </div>
+    );
+};
+
+export default Homepage;
+
+
